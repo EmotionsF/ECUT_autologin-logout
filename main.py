320a7ecf29e41ad320c7910b7414404a5b74e966
Katayo Kizuna <washiobi3109@gmail.com>
2024-11-29 23:31:41 +0800

    second commit

--- main.py
+++ main.py
@@ -5,6 +5,7 @@
 # @Time : 2024/11/29 19:00 傍晚（改进版）
 from autoLogin import *
 from manualLogout import *
+from reLog import *
 pre = "##########################################\n" \
       "@author : 原作者hailong学长 此为 Citrus de Hikaru 改进版\n" \
       "@Github（hailong）: https://github.com/hailong-z\n" \
@@ -17,11 +18,15 @@ if __name__ == "__main__":
     print("选择操作：")
     print("1. 自动登录")
     print("2. 手动登出")
+    print("3. 重新登录")
     choice = input("请输入数字 (1或2)：")
 
     if choice == "1":
         autoLogin()
     elif choice == "2":
         manualLogout()
+    elif choice == "3":
+        reLog()
     else:
-        print("无效的选择！")
+        print("无效的选择！你等会再来把")
+        time.sleep(2)
